cc023b2700b4a18264185ef6f478554807ca9817
soda3024 <cnswkdi9133@naver.com>
2021-11-05 19:39:36 +0900

    .

---
+++ dbot.py
@@ -0,0 +1,15 @@
+import discord
+import os
+from discord.ext import commands
+ 
+bot = commands.Bot(command_prefix=',')
+ 
+@bot.event
+async def on_ready():
+    print('다음으로 로그인합니다: ')
+    print(bot.user.name)
+    print('connection was succesful')
+    await bot.change_presence(status=discord.Status.online,  activity=discord.Game(name="게임"))
+
+     
+bot.run(os.environ['token'])
