4860f480764cbdb9a7d5905230faf13ebcb74fc9
soda3024 <cnswkdi9133@naver.com>
2021-11-06 01:44:17 +0900

    Update dbot.py

--- dbot.py
+++ dbot.py
@@ -9,7 +9,7 @@ async def on_ready():
     print('다음으로 로그인합니다: ')
     print(bot.user.name)
     print('connection was succesful')
-    await bot.change_presence(status=discord.Status.online,  activity=discord.Game(name="게임"))
+    await bot.change_presence(status=discord.Status.online,  activity=discord.Game(name="노가다"))
 
 @bot.command()
 async def 청소(ctx, amount : int):
