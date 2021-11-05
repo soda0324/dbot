39b9cd0457d8936aad4d8405c1c12f0e6d4c4e49
soda3024 <cnswkdi9133@naver.com>
2021-11-06 02:07:52 +0900

    Update dbot.py

--- dbot.py
+++ dbot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import random
 from discord.ext import commands
  
 bot = commands.Bot(command_prefix=',')
@@ -17,6 +18,28 @@ async def 청소(ctx, amount : int):
 
 async def 핑(ctx):
     await ctx.send(f'```ping : {round(bot.latency * 1000)}ms```')
+
+@bot.event()
+async def on_message(message):
+    if message.content.startswith("!투표"):
+        vote = message.content[4:].split("/")
+        channel = message.channel
+        await channel.send("투표를 시작합니다.")
+        for i in range(0, len(vote)):
+            lastsend = await channel.send("```" + vote[i] + "```")
+            await lastsend.add_reaction('✅')
+            await lastsend.add_reaction('❌') 
+
+    if message.author == bot.user:
+        return
+    
+    if message.content.startswith('!골라'):
+        message.content = message.content.replace("!골라 ","")
+        messagesplit = message.content.split(",")
+        msg = random.choice(messagesplit)+'을 골랐습니다.'
+        await message.channel.send(msg)
+
+    await app.process_commands(message)
 
      
 bot.run(os.environ['token'])
