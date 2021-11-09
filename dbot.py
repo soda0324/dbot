8fe3ac2d20598e9de77840544050203809eecc64
soda3024 <cnswkdi9133@naver.com>
2021-11-09 21:06:46 +0900

    Update dbot.py

--- dbot.py
+++ dbot.py
@@ -1,9 +1,7 @@
 import discord
 import os
-from discord import guild
-from discord.channel import VoiceChannel
 from discord.ext import commands
-import youtube_dl
+
  
 bot = commands.Bot(command_prefix=',')
  
